c833dc5f15e4a0c71df31732f9311dd46c8765d9
book3 <y.karagulov@gmail.com>
2018-04-23 09:31:02 -0400

    handeled oauth

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport'); 
+
+
+//passport config
+require('./config/passport')(passport);
+
+//Load routes
+const auth= require('./routes/auth');
 
 const app = express();
 
     app.get('/',(req,res)=>{
       res.send('sdkjfsldk');
     });
+
+
+//use routes
+    app.use('/auth',auth);
+
+
 
 
 const port= process.env.PORT || 5000;
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports={
+  mongoURI:'mongodb://storybook-dev:storybook-dev@ds151809.mlab.com:51809/storybook-dev',
+  googleClientID:'443044083159-kgrip41i5gcbm5ndnol2vk13jrsvdjv7.apps.googleusercontent.com',
+  googleClientSecret:'4FDc_7meiFzPbZZr0xqrmT0q'
+}
